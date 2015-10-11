4e25ff43e5ca886dda577657af53e59195b83cd0
bryn <baritompa@gmail.com>
2015-10-11 12:54:13 +0900

    Update bare bones with private app key, template,  and readme

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
@@ -10,10 +11,11 @@ app = Flask(__name__)
 @app.route('/')
 def test():
     print "test"
-    return render_template('test.html')
+    return render_template('index.html', content = "test")
 
 
 if __name__ == '__main__':
-    app.secret_key = '71864813625223673536721504869408395782255281482800985325'
+    app.secret_key = json.loads(
+        open('private/secrets.json', 'r').read())['app']['secret_key']
     app.debug = True
     app.run(host='localhost', port=5000)
